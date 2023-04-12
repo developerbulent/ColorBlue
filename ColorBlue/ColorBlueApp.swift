966b45dcaebe76c0bf6780c46f13cfc4f0e5df8d
developerbulent <bulentmailforwork@gmail.com>
2023-04-12 16:53:15 +0300

    Initial Commit

---
+++ ColorBlue/ColorBlueApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorBlueApp.swift
+//  ColorBlue
+//
+//  Created by Bulent on 12.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorBlueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
